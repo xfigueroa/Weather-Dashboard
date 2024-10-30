de025414e58c524a5007e508589ecaf865a08643
Xavier Figueroa <xfigueroa28@gmail.com>
2024-10-30 13:13:28 -0400

    setting server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,9 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
+app.use(express.static('client'))
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 
