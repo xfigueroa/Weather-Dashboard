f500518ab5cb462e1cf3c2c18cacc93cde8b6da8
Xavier Figueroa <xfigueroa28@gmail.com>
2024-11-12 17:51:07 -0500

    revert tscofing

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder 
-app.use(express.static('client/dist'))
+app.use(express.static('./client/dist'))
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data 
 app.use(express.json());
