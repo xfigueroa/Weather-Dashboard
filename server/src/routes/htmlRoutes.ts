143fbd1ae62c1c8508b280740dc433cbfa2d7627
Xavier Figueroa <xfigueroa28@gmail.com>
2024-11-15 17:06:36 -0500

    update to render

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,13 +1,15 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Router } from 'express';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// TODO: Define route to serve index.html ------Done??
+// TODO: Define route to serve index.html 
 router.get('/', (_req, res) => {
     const indexPath = path.join(__dirname, '../../index.html');
     res.sendFile(indexPath);
   });
 export default router;
+ 
